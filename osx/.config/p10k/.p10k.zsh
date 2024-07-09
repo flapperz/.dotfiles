a42bead14568b975514e26ef9207f3b2a51540a1
flapperz <flap.atyz@gmail.com>
2024-07-09 18:34:36 +0700

    zsh: lean git status

--- osx/.config/p10k/.p10k.zsh
+++ osx/.config/p10k/.p10k.zsh
@@ -438,20 +438,18 @@
       res+=" ${modified}wip"
     fi
     
-    # flap add paren
-    res="${clean}(${res}${clean})"
 
     # flap: disable status line
-    # # ⇣42 if behind the remote.
-    # (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
-    # # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
-    # (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && res+=" "
-    # (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
-    # # ⇠42 if behind the push remote.
-    # (( VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
-    # (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" "
-    # # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
-    # (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && res+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
+    # ⇣42 if behind the remote.
+    (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
+    # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
+    (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && res+=" "
+    (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
+    # ⇠42 if behind the push remote.
+    (( VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
+    (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" "
+    # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
+    (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && res+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
     # # *42 if have stashes.
     # (( VCS_STATUS_STASHES        )) && res+=" ${clean}*${VCS_STATUS_STASHES}"
     # # 'merge' if the repo is in an unusual state.
@@ -473,6 +471,8 @@
     # # in this case.
     # (( VCS_STATUS_HAS_UNSTAGED == -1 )) && res+=" ${modified}─"
     
+    # flap add paren
+    res="${clean}(${res}${clean})"
 
     typeset -g my_git_format=$res
   }
